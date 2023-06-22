c1affc9c646722edbc642c0e306968c57dea9d88
christer <eber.christer@gmail.com>
2023-06-22 19:52:31 +0200

    jessica code integrated

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <filesystem>
 #include <iostream>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
 using namespace std;
 
 #define VIDEOPATH "/mnt/c/Users/eberc/Desktop/all/Edu/sem6/AR/ARchitecture/resources/MarkerMovie.MP4"
@@ -13,6 +17,18 @@ using namespace std;
 
 #define CAM_MTX (cv::Mat_<float>(3, 3) << 1000, 0.0, 500, 0.0, 1000, 500, 0.0, 0.0, 1.0)
 #define CAM_DIST (cv::Mat_<float>(1, 4) << 0, 0, 0, 0)
+
+
+void setPerspective(float fovY, float aspect, float zNear, float zFar)
+{
+    float top = tan(fovY * 0.5 * (3.14159265358979323846 / 180.0)) * zNear;
+    float bottom = -top;
+    float left = aspect * bottom;
+    float right = aspect * top;
+
+    glFrustum(left, right, bottom, top, zNear, zFar);
+}
+
 
 int main(int argc, char const *argv[]){
     bool debug = false;
@@ -22,6 +38,26 @@ int main(int argc, char const *argv[]){
             cout << "[prog] Debug mode enabled" << endl;
         }
     }
+
+    cv::Mat frame;
+    cv::VideoCapture cap("http://192.168.0.7:4747/video", cv::CAP_FFMPEG);
+
+    if (!cap.isOpened()){
+        cout << "[CV] No Webcam detected, searching for video file" << endl;
+        cap.open(VIDEOPATH, cv::CAP_FFMPEG);
+        if (!cap.isOpened()){
+            cout << "[CV] No video file detected, exiting" << endl;
+            exit(0);
+        }
+        cout << "[CV] Video file detected" << endl;
+    }
+
+    cout << "=========================================" << endl;
+    cout << "[CV] Video Metadata: " << endl;
+    cout << "\tFrame Count: " << cap.get(cv::CAP_PROP_FRAME_COUNT) << endl;
+    cout << "\tFrame Rate: " << cap.get(cv::CAP_PROP_FPS) << endl;
+    cout << "\tFrame Dimension: " << cap.get(cv::CAP_PROP_FRAME_WIDTH) << "x" << cap.get(cv::CAP_PROP_FRAME_HEIGHT) << endl;
+    cout << "=========================================" << endl;
 
     // read the files in a directory
     vector<string> markerPaths;
@@ -34,29 +70,27 @@ int main(int argc, char const *argv[]){
     MarkerDict dict = MarkerDetection::constructMarkerDictionary(markerPaths);
     cout << "=========================================" << endl;
 
-    cv::Mat frame;
-    cv::VideoCapture cap("http://192.168.0.7:4747/video", cv::CAP_FFMPEG);
-
-    if (!cap.isOpened()){
-        cout << "[prog] No Webcam detected, searching for video file" << endl;
-        cap.open(VIDEOPATH, cv::CAP_FFMPEG);
-        if (!cap.isOpened()){
-            cout << "[prog] No video file detected, exiting" << endl;
-            exit(0);
-        }
-        cout << "[prog] Video file detected" << endl;
+    // Initialize GLFW
+    if (!glfwInit()){
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
     }
+    cout << "[GLFW] GLFW initialized" << endl;
 
-    cout << "=========================================" << endl;
-    cout << "[prog] Video Metadata: " << endl;
-    cout << "\tFrame Count: " << cap.get(cv::CAP_PROP_FRAME_COUNT) << endl;
-    cout << "\tFrame Rate: " << cap.get(cv::CAP_PROP_FPS) << endl;
-    cout << "\tFrame Dimension: " << cap.get(cv::CAP_PROP_FRAME_WIDTH) << "x" << cap.get(cv::CAP_PROP_FRAME_HEIGHT) << endl;
-    cout << "=========================================" << endl;
+    GLFWwindow* window = glfwCreateWindow(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT), "render", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "[GLFW] Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
 
     while(cap.read(frame)){
         cv::Mat frame_clone = frame.clone();
         cv::Mat frame_pose = frame.clone();
+        cv::Mat frame_render = frame.clone();
 
         vector<MarkerResult> results = MarkerDetection::detectMarker(frame_clone, dict, 0, debug);
 
@@ -64,6 +98,39 @@ int main(int argc, char const *argv[]){
             cv::putText(frame_clone, to_string(results[i].index), results[i].corners[0], cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(32, 32, 255), 2);
             cv::drawContours(frame_clone, vector<vector<cv::Point>>{results[i].corners}, 0, cv::Scalar(0, 255, 32), 1);
         }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Set up the camera
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        setPerspective(45.0, 1.0, 1.0, 100.0);
+        //glOrtho(0.0, frame_render.cols, 0.0, frame_render.rows, -1.0, 1.0);
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        //glTranslatef(0.0, 0.5, -10.0);
+        glTranslatef(0.0, 0.0, -5.0); //correct value
+        
+        
+        // Convert the frame to OpenGL texture format
+        cv::flip(frame_render, frame_render, 0);  // Flip vertically
+        cv::cvtColor(frame_render, frame_render, cv::COLOR_BGR2RGB);
+
+        // Set up the texture mapping
+        glEnable(GL_TEXTURE_2D);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_render.cols, frame_render.rows, 0, GL_RGB, GL_UNSIGNED_BYTE, frame_render.data);
+
+        // Draw a rectangle with the webcam frame as the background
+        glColor3f(1.0, 1.0, 1.0);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0.0, 0.0); glVertex3f(-2.5, -2.5, 0.0);
+        glTexCoord2f(1.0, 0.0); glVertex3f(2.5, -2.5, 0.0);
+        glTexCoord2f(1.0, 1.0); glVertex3f(2.5, 2.5, 0.0);
+        glTexCoord2f(0.0, 1.0); glVertex3f(-2.5, 2.5, 0.0);
+        glEnd();
 
         // Pose Estimation        
         for (MarkerResult& res : results){
@@ -80,6 +147,26 @@ int main(int argc, char const *argv[]){
                 cv::putText(frame_pose, "y", projectedPoints[2], cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 1);
                 cv::putText(frame_pose, "z", projectedPoints[3], cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(255, 0, 0), 1);
             }
+            GLfloat scalingFactor = 2.0f;
+        
+            glPushMatrix();
+            glTranslatef(-1.0, 0.0, 0.0);
+            glBegin(GL_TRIANGLES);
+            glColor3f(1.0f, 0.0f, 0.0f);
+            glVertex2f(projectedPoints[0].x/frame_render.cols * scalingFactor, 1 - projectedPoints[0].y/frame_render.rows * scalingFactor);  // Vertex 1
+            glVertex2f(projectedPoints[1].x/frame_render.cols * scalingFactor, 1 - projectedPoints[1].y/frame_render.rows * scalingFactor);  // Vertex 2
+            glVertex2f(projectedPoints[3].x/frame_render.cols * scalingFactor, 1 - projectedPoints[3].y/frame_render.rows * scalingFactor);  // Vertex 3
+            glEnd();
+
+            
+            glBegin(GL_TRIANGLES);
+            glColor3f(0.0f, 1.0f, 0.0f);
+            glVertex2f(projectedPoints[0].x/frame_render.cols * scalingFactor, 1 - projectedPoints[0].y/frame_render.rows * scalingFactor);  // Vertex 1
+            glVertex2f(projectedPoints[2].x/frame_render.cols * scalingFactor, 1 - projectedPoints[2].y/frame_render.rows * scalingFactor);  // Vertex 2
+            glVertex2f(projectedPoints[3].x/frame_render.cols * scalingFactor, 1 - projectedPoints[3].y/frame_render.rows * scalingFactor);  // Vertex 3
+            glEnd();
+             
+            glPopMatrix();
         }
 
 
@@ -101,7 +188,19 @@ int main(int argc, char const *argv[]){
             break;
         }
 
+        glDisable(GL_TEXTURE_2D);
+
+        glFlush();
+        glfwSwapBuffers(window);
+        
+        
+        //renderScene(window,frame_pose);
+        glfwPollEvents();
+
     }
+    cap.release();
+
+    glfwTerminate();
 
     return 0;
 }
